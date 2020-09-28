3a9f0e1e86794c8885ed5f02b6010d04ea7a95de
olliesmith3 <olliesmith3@rocketmail.com>
2020-09-28 15:28:12 +0100

    updated .create method on  space.rb

--- lib/space.rb
+++ lib/space.rb
@@ -1,19 +1,23 @@
 class Space
 
-  attr_reader :username, :description, :price
+  attr_reader :username, :description, :price, :id
 
-  def initialize(username, description, price)
+  def initialize(id, username, description, price)
+    @id = id
     @username = username
     @description = description
     @price = price
   end
 
   def self.create(username, description, price)
-    Space.new(username, description, price)
+    DatabaseConnection.setup(airbnb_test)
+
+    result = DatabaseConnection.query("INSERT INTO spaces (username, description, price) VALUES ('#{username}', '#{description}', '#{price}' RETURNING id;")
+    Space.new(result[0]['id'], username, description, price)
   end
 
   def self.all
-    
+  end
 
 
 
