8d143ce8b91ce11b1e2db9d023221b83223b45e0
olliesmith3 <olliesmith3@rocketmail.com>
2020-09-29 14:12:14 +0100

    added request class
    Also added instructions for setting up bookmarks table in database

---
+++ lib/request.rb
@@ -0,0 +1,19 @@
+class Request
+
+  attr_reader :id, :date, :guest_id, :space_id, :approved
+
+  def initialize(id, date, guest_id, space_id)
+    @id = id
+    @date = date
+    @guest_id = guest_id
+    @space_id = space_id
+    @approved = 0
+  end
+
+  # def self.create(date, guest_id, space_id)
+  #   DatabaseConnection.setup('airbnb_test')
+
+  #   result = DatabaseConnection.query("INSERT INTO bookings (date, guest_id, space_id, approved) VALUES ('#{date}', '#{guest_id}', '#{space_id}', '0') RETURNING id;") #the BIT data type allows us to use 1 for true and 0 for false.
+  #   Request.new(result[0]['id'], date, guest_id, space_id)
+  # end
+end
---
+++ spec/units/request_spec.rb
@@ -0,0 +1,20 @@
+require 'request'
+
+describe Request do
+  describe '#new' do
+    it 'a new request has id, proposed date, guest-id and space-id' do
+      request = Request.new(45, '30/09/2020', 75, 2)
+      expect(request.id).to eq 45
+      expect(request.date).to eq '30/09/2020'
+      expect(request.guest_id).to eq 75
+      expect(request.space_id).to eq 2
+      expect(request.approved).to eq false
+    end
+  end
+
+  # describe '.create' do
+  #   it 'adds a row to the bookings table and returns a Request object' do
+      
+  #   end
+  # end
+end
