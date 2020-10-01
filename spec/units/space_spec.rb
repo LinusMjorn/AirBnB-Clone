e4199f6c4a57bc3c0ec8da0ff29964179a382a01
ollie beney <ombeney@gmail.com>
2020-10-01 14:42:34 +0100

    fixed some rspec

--- spec/units/space_spec.rb
+++ spec/units/space_spec.rb
@@ -17,8 +17,10 @@ describe Space do
 
   describe '.all' do
     it 'shows all spaces' do
-      Space.create(15, 'seafront room', 400, ['2021-01-30', '2021-02-20', '2021-02-22'])
-      Space.create(666, 'bachelor pad', 50, ['2021-01-30', '2021-02-20', '2021-02-22'])
+      linus = User.store("password2","linus","linus@linus.com")
+      johnny = User.store("password2","johnny","linus@linus.com")
+      Space.create(linus.id, 'seafront room', 400, ['2021-01-30', '2021-02-20', '2021-02-22'])
+      Space.create(johnny.id, 'bachelor pad', 50, ['2021-01-30', '2021-02-20', '2021-02-22'])
       expect(Space.all[0].description).to eq 'bachelor pad'
       expect(Space.all[1].description).to eq 'seafront room'
     end
@@ -27,8 +29,8 @@ describe Space do
   describe '.find' do
     it 'finds a space with using the space id' do
       user = User.store("password","linus","linus@linus.com")
-      entry = Space.create(user.id, 'seafront room', 400)
-      space = Space.new(entry[0]['id'],user.id, 'seafront room', 400)
+      entry = Space.create(user.id, 'seafront room', 400, ['2021-01-30', '2021-02-20', '2021-02-22'])
+      space = Space.new(entry.id,user.id, 'seafront room', 400, ['2021-01-30', '2021-02-20', '2021-02-22'])
       expect(Space.find(space.id).id).to eq space.id
    end
  end
