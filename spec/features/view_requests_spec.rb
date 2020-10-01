b2a01ab84a4921614f8880aa80ca86ca48816f6c
ollie beney <ombeney@gmail.com>
2020-10-01 13:54:59 +0100

    fixed two rspec tests that no longer had correct targets

--- spec/features/view_requests_spec.rb
+++ spec/features/view_requests_spec.rb
@@ -3,11 +3,11 @@ require 'setup_test_database.rb'
 feature 'see list of requests relevant to the user' do
   scenario 'User goes to request list page and sees a list of the requests theyve made' do
     signup_bear1
-    login_bear1
+    #login_bear1
     add_new_listing
+    visit("/")
     visit '/sessions/logout'
     signup_happyotter
-    login_happyotter
     first(:button, 'Request Space', minimum: 1).click
     #first('.space').click_button 'Request Space'
     click_button 'View My Spaces'
--- spec/units/space_spec.rb
+++ spec/units/space_spec.rb
@@ -1,24 +1,29 @@
 require 'space'
 require 'user'
+require 'database_connection'
 
 
 describe Space do
   describe '.create' do
-    it 'creates a new listing' do
-      seafront = Space.create(15, 'seafront room', 400)
+    it 'returns the id of a newly created space in the database' do
+      seafront_id = Space.create(15, 'seafront room', 400)
+      seafront = Space.new(seafront_id, 15,'seafront room', 400)
       expect(seafront).to be_a(Space)
       expect(seafront.description).to eq 'seafront room'
-      expect(seafront.userid).to eq 15
+      expect(seafront.username).to eq 15
       expect(seafront.price).to eq 400
     end
   end
 
   describe '.all' do
     it 'shows all spaces' do
-      Space.create(15, 'seafront room', 400)
-      Space.create(666, 'bachelor pad', 50)
-      expect(Space.all[0].description).to eq 'bachelor pad'
-      expect(Space.all[1].description).to eq 'seafront room'
+      user = User.store("password","linus","linus@linus.com")
+      
+      Space.create(user.id, 'seafront room', 400)
+      user2 = User.store("password2","linus","linus@linus.com")
+      Space.create(user2.id, 'bachelor pad', 50)
+      expect(Space.all.first.description).to eq 'bachelor pad'
+      expect(Space.all.last.description).to eq 'seafront room'
     end
   end
 
